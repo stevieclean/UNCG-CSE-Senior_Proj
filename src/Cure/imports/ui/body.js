3ddc587c21e0f73537088bc5474dd149c6e65720
Brian Goughnour <38028922+bgoughn@users.noreply.github.com>
2019-04-09 15:52:22 -0400

    Sweet massage passing

--- src/Cure/imports/ui/body.js
+++ src/Cure/imports/ui/body.js
@@ -76,8 +76,12 @@ export default class GoalTracker {
     handleMessage(message) {
         if (message.dest == Destination["GOALTRACKER"]) {
             // if is true the goaltracker handles the message
-            console.log("Got dat shit!");
+            console.log("Got dat bruh!");
             console.log(message);
+            Goals.insert({
+                text: message,
+                createdAt: new Date()
+            });
 
         }
     }
