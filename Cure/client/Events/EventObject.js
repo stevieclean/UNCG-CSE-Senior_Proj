4d7a4de84269005123367f140f43f3543fcc4b25
Joshua <jdbrown7@uncg.edu>
2019-04-25 20:08:09 -0600

    Removing unnecessary folders

--- Cure/client/Events/EventObject.js
+++
@@ -1,5 +0,0 @@
-class EventObject {
-	constructor() {
-		//TODO: Make this accept generic objects through subclasses to this class.
-	}
-}
--- Cure/server/main.js
+++
@@ -1,2 +0,0 @@
-import '../imports/api/goals.js';
-//End import for the goals
